f4c23d1c51ce7848fb9a0b28caac613fec3a9dd7
Lance <lance.cabance@mail.utoronto.ca>
2026-04-22 22:15:54 -0400

    no more

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/P2026/" : "/",
   server: {
     host: "::",
     port: 8080,
